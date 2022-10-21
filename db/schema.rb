c3b4054841de09bff6f0ff65ca756965c3bc730b
braincee <kwesi938@gmail.com>
2022-10-21 18:03:18 +0100

    Add Author reference to like

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_165833) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_170037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_165833) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -51,5 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_165833) do
 
   add_foreign_key "comments", "authors"
   add_foreign_key "comments", "posts"
+  add_foreign_key "likes", "authors"
   add_foreign_key "posts", "authors"
 end
