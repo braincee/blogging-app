75c3e37f9d4301aee7ea9ca8247cd9c64191d214
braincee <kwesi938@gmail.com>
2022-10-21 17:53:26 +0100

    Add author reference to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_164031) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_165003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_164031) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -30,6 +36,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_164031) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "posts", "authors"
 end
