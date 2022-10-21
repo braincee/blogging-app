2c9f21cc75da9f5da8dbacd034a62b9cfcfac320
braincee <kwesi938@gmail.com>
2022-10-21 18:06:23 +0100

    Add post reference to like

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_170037) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_170455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_170037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,5 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_170037) do
   add_foreign_key "comments", "authors"
   add_foreign_key "comments", "posts"
   add_foreign_key "likes", "authors"
+  add_foreign_key "likes", "posts"
   add_foreign_key "posts", "authors"
 end
