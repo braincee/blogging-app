5f1b10498dd7a69334fc63c46fc32382fa1b3736
braincee <kwesi938@gmail.com>
2022-10-21 17:43:19 +0100

    create post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_163147) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_164031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_163147) do
     t.string "photo"
     t.text "bio"
     t.integer "post_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
