8754aa915c7af80e9b93e068a21185f36f43d6eb
braincee <kwesi938@gmail.com>
2022-10-21 17:57:24 +0100

    Add post reference to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_165003) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_165531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_165003) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -40,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_165003) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "comments", "authors"
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "authors"
 end
