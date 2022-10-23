6f170666e244d2950d68c144522f73f08e6a30bc
braincee <kwesi938@gmail.com>
2022-10-23 02:48:29 +0100

    Add validation to post counter on author class to be an integert >= 0

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,7 @@ class Author < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     posts.order(created_at: desc).limit(3)
