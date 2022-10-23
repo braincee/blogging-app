fbbeac2652f61357f5ed8d68fc46f81b1c16ae47
braincee <kwesi938@gmail.com>
2022-10-23 04:13:54 +0100

    add unit testing for like class model, methods and validations

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject { Like.new(author_id: 3, post_id: 5) }
+
+  before { subject.save }
+
+  it 'should return author_id is present' do
+      subject.author_id = false
+      expect(subject).to_not be_valid
+    end
+
+  it 'should return author id be an integer' do
+    subject.author_id = 'Steve'
+    expect(subject).to_not be_valid
+  end
+
+   it 'should return if post_id is present' do
+      subject.post_id = nil
+      expect(subject).to_not be_valid
+    end
+
+  it 'Should post id be an integer' do
+    subject.post_id = 'Gloria'
+    expect(subject).to_not be_valid
+  end
+end
