e8a744892f981d04a1dd528154dd096408679c44
braincee <kwesi938@gmail.com>
2022-10-23 04:02:19 +0100

    Add unit test for comment class model, methods and validations

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+    subject { Comment.new(text: 'My comment', author_id: 2) }
+
+    before { subject.save }
+
+    it 'should return text is present' do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+
+   it 'should return author_id is an integer' do
+    subject.author_id = "Steve"
+    expect(subject).to_not be_valid
+    end
+
+    it 'should return author_id being valid' do 
+        subject.author_id = false
+        expect(subject).to_not be_valid
+    end
+
+    it 'shoul;d return author_id not being valid' do
+        subject.author_id = true
+        expect(subject).to_not be_valid
+    end
+end
