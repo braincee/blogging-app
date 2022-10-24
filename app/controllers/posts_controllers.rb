7239ed50fd79f5fc98d7297e205d3ffac3be9513
braincee <kwesi938@gmail.com>
2022-10-24 12:14:57 +0100

    Add modification to post controllers

--- app/controllers/posts_controllers.rb
+++ app/controllers/posts_controllers.rb
@@ -1,7 +1,12 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
     def index
+        @user = User.find(params[:user_id])
+        @posts = @user.posts
     end
 
     def show 
+        @user = User.find(params[:user_id])
+        @posts = Post.find_by(user_id: params[:user_id], id:
+        params[:id])
     end
 end
