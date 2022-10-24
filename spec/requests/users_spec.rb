b0494b9163b79202d965a4e348de336bd507b879
braincee <kwesi938@gmail.com>
2022-10-24 20:27:40 +0100

    Add test to render user index template

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,5 +8,8 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    
+     it 'should render index template' do
+      get users_index_path
+      expect(response).to render_template(:index)
+    end
 end
