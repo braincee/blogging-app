238779ce45ebbddaf80d8143c5b445df0c056531
braincee <kwesi938@gmail.com>
2022-10-25 14:34:35 +0100

    Add setup for post views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
+
+  root 'users#index'
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
