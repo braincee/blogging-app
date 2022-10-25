561e56712783bebd6e2aaa19b80ce1454bc1fb71
braincee <kwesi938@gmail.com>
2022-10-25 14:40:31 +0100

    Add views to user's show html page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   def index
+    @users = User.all
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
+    @users = User.all
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
