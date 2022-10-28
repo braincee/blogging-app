fe478adbab8c26b4ccb77d110a9eaed0f9ff968f
braincee <kwesi938@gmail.com>
2022-10-28 18:32:02 +0100

    Add styling to views files

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,25 @@
 class LikesController < ApplicationController
+  def new
+    like = Like.new
+    respond_to do |format|
+      format.html { render :new, locals: { like: } }
+    end
+  end
+
   def create
-    @id = current_user.id
+    user = User.find(params[:user_id])
     post = Post.find(params[:post_id])
-    like = Like.new(user: current_user, post:)
+    like = Like.new(user:, post:)
     respond_to do |format|
       format.html do
         if like.save
           like.counter_updater
-          flash[:success] = 'Liked'
+          flash[:notice] = 'Liked'
+          redirect_to "/users/#{user.id}/posts/#{post.id}"
         else
           flash.now[:error] = 'Error: Like could not be saved'
+          render :new, locals: { like: }
         end
-        redirect_to "/users/#{@id}/posts/#{params[:post_id]}"
       end
     end
   end
