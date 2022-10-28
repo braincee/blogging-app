d77c5a6df95258cf6f182281a75a91320808ea86
braincee <kwesi938@gmail.com>
2022-10-28 17:50:37 +0100

    Add :create method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,20 @@ class PostsController < ApplicationController
     end
   end 
 
+  def create 
+    @id = current_user
+    post = Post.new (title: params[:title], text: params[:text], user: current_user, comments_counter: 0, likes_counter: 0)
+    respond_to do |format|
+      format.html do 
+        if post.save
+          post.update_post_counter
+          flash[:success] = Post 'successfully saved'
+          redirect_to user_post_path
+        end
+      end
+    end
+  end
+
   def show
     @users = User.all
     @user = User.find(params[:user_id])
