aa58cb971016364c8ec51613cf71f9e017e16fac
braincee <kwesi938@gmail.com>
2022-10-28 17:55:55 +0100

    Add :new method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ActionController
+     def new
+        comment = Comment.new
+        respond_to do |format|
+            format.html { render :new, locals: { comment: } } 
+        end
+    end
 end
