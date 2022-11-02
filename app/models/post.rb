28e2977cc35cb2dbd3ccf91a83ef64fc2127327c
braincee <kwesi938@gmail.com>
2022-11-02 14:28:57 +0000

    Add modifications to views

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,18 +4,16 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: {
-    only_integer: true, greater_than_or_equal_to: 0
-  }
+  validates :comments_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  validates :likes_counter, numericality:
-    { only_integer: true, greater_than_or_equal_to: 0 }
-
-  def update_post_counter
-    user.update(posts_counter: user.posts.length)
+  def counter_updater
+    user.update(post_counter: user.posts.length)
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+  def last_five_comments
+    comments.last(5)
   end
 end
