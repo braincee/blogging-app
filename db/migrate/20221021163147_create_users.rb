41690be5721a3620eba27d14e9aaf78860f4abbb
braincee <kwesi938@gmail.com>
2022-11-02 13:52:21 +0000

    Add user devise generator

--- db/migrate/20221021163147_create_users.rb
+++ db/migrate/20221021163147_create_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class CreateAuthors < ActiveRecord::Migration[7.0]
+class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :authors do |t|
+    create_table :users do |t|
       t.string :name
       t.string :photo
       t.text :bio
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_170455) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "post_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
